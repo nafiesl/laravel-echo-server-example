2aa5450a1f390d765775c657b11d40f664c14e97
Nafies Luthfi <nafiesl@gmail.com>
2018-12-03 20:46:25 +0800

    Add task api route and controller actions

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function fetchAll()
+    {
+        return Task::all();
+    }
+
+    public function store(Request $request)
+    {
+        Task::create($request->all());
+
+        return response()->json('added');
+    }
+
+    public function delete(Task $task)
+    {
+        $task->delete();
+
+        return response()->json('deleted');
+    }
 }
